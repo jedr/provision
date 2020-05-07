229cc0760a3eddf2d1aac2d861952f51d0515fc5
jedr <andrzej.stencel@gmail.com>
2020-05-07 19:36:52 +0200

    Split windows script into base and work

---
+++ windows/work.ps1
@@ -0,0 +1,11 @@
+choco install -y git
+git config --global core.autocrlf false
+choco install -y googlechrome
+choco install -y postman
+choco install -y virtualbox
+choco install -y vagrant
+choco install -y vscode
+code --install-extension vscodevim.vim
+code --install-extension eamodio.gitlens
+code --install-extension editorconfig.editorconfig
+code --install-extension github.vscode-pull-request-github
