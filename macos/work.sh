d83967de24f971cb6ffd72184cfc36d30407b590
jedr <andrzej.stencel@gmail.com>
2020-09-23 10:16:59 +0200

    Add MacOS specific VS Code fix for repeating keystrokes in VsVim

---
+++ macos/work.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# https://stackoverflow.com/questions/39972335/how-do-i-press-and-hold-a-key-and-have-it-repeat-in-vscode
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
