44a3a6aea3f29bf893ec4b2fab1e62a93676e9f9
jedr <andrzej.stencel@gmail.com>
2020-09-23 13:14:14 +0200

    Install Homebrew, some basic apps on MacOS

---
+++ macos/base.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew cask install firefox
+
+# https://rectangleapp.com/ - Move and resize windows in macOS using keyboard shortcuts or snap areas
+brew cask install rectangle
--- macos/work.sh
+++ macos/work.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+brew cask install code
+
+brew cask install virtualbox
+brew cask install vagrant
+
 # https://stackoverflow.com/questions/39972335/how-do-i-press-and-hold-a-key-and-have-it-repeat-in-vscode
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
