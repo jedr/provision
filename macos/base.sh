a6d8b91fec1c83f732fd6f162885039764bb9b17
jedr <andrzej.stencel@gmail.com>
2020-10-22 21:40:37 +0200

    Add Karabiner for MacOS

--- macos/base.sh
+++ macos/base.sh
@@ -7,3 +7,6 @@ brew cask install firefox
 
 # https://rectangleapp.com/ - Move and resize windows in macOS using keyboard shortcuts or snap areas
 brew cask install rectangle
+
+# https://karabiner-elements.pqrs.org/index.html - Switch Fn with Ctrl keys
+brew cask install karabiner-elements
