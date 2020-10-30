4df8539e809f64de044777840234fd6decdb4c08
jedr <andrzej.stencel@gmail.com>
2020-10-30 08:54:40 +0100

    shfmt

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,6 +6,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get upgrade --yes
 apt-get install --yes \
-    net-tools # provides ifconfig
+  net-tools # provides ifconfig
 
-echo "export EDITOR=vim" >> /home/vagrant/.bashrc
+echo "export EDITOR=vim" >>/home/vagrant/.bashrc
