3c543edc5a2dde164d25482141b9c303c7884d0d
jedr <andrzej.stencel@gmail.com>
2020-11-29 22:01:01 +0000

    Fix ruby.sh differently

--- vagrant/provision/install/ruby.sh
+++ vagrant/provision/install/ruby.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -exuo pipefail
 
+# Prepare PATH so that `rbenv-doctor` won't fail.
+PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+
 # Install rbenv from https://github.com/rbenv/rbenv-installer#rbenv-installer
-# The `|| true ` suffix is needed, because `rbenv-installer` calls `rbenv-doctor`, which fails. See https://github.com/rbenv/rbenv-installer/pull/23.
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash || true
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
 
 cat <<EOT >> ~/.profile
-if [ -d "\$HOME/.rbenv/bin" ] ; then
-    PATH="\$HOME/.rbenv/bin:\$PATH"
-fi
+PATH="\$HOME/.rbenv/bin:\$PATH"
+eval "\$(rbenv init -)"
 EOT
-
-echo -e "eval \"\$(rbenv init -)\"" >> ~/.bashrc
 
 # Install dependencies to compile rubies - based on https://www.itzgeek.com/post/how-to-install-ruby-on-rails-on-ubuntu-20-04/#install-dependencies
 sudo apt-get update
