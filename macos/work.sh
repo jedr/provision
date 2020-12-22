f07e92665fb19b41d61e05ebe9235697daa76280
jedr <andrzej.stencel@gmail.com>
2020-12-22 13:52:11 +0100

    Add kubectl, k9s to macos/work

--- macos/work.sh
+++ macos/work.sh
@@ -7,3 +7,6 @@ brew cask install vagrant
 
 # https://stackoverflow.com/questions/39972335/how-do-i-press-and-hold-a-key-and-have-it-repeat-in-vscode
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+brew install kubectl
+brew install k9s
