8a275f743bbda5b7f1f5cab8bd91e032e57e9d88
jedr <andrzej.stencel@gmail.com>
2021-02-25 20:32:17 +0100

    vagrant: Update dotnet installer from 3.1 to 5.0

--- vagrant/provision/install/dotnet.sh
+++ vagrant/provision/install/dotnet.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+# Taken from https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#2004-
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
-sudo apt-get install -y dotnet-sdk-3.1
+sudo apt-get install -y dotnet-sdk-5.0
