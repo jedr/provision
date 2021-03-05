2bbdff0d34705905f1f6d71882c6fde0fcffeab0
jedr <andrzej.stencel@gmail.com>
2021-03-05 21:29:34 +0000

    vagrant: Update `go.sh` to use tarball
    The `getgo` method was messing up Bash startup scripts.

--- vagrant/provision/install/go.sh
+++ vagrant/provision/install/go.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -exuo pipefail
 
-# https://github.com/golang/tools/tree/master/cmd/getgo
-curl -LO https://get.golang.org/$(uname)/go_installer && chmod +x go_installer && ./go_installer && rm go_installer
+GO_VERSION="1.16"
+wget "https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz"
+sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+rm go${GO_VERSION}.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+PATH=$PATH:/usr/local/go/bin
