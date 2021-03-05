627a475237f07f14596242ee04677a5c19985d65
jedr <andrzej.stencel@gmail.com>
2021-03-05 21:30:14 +0000

    vagrant-boxes: Add ubuntu-zsh

---
+++ vagrant-boxes/ubuntu-zsh/provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get --yes upgrade
+
+# Install zsh
+
+# Install Oh My Zsh
+
+# Install and configure powerlevel10k
+
+# Add plugins
