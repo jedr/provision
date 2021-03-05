8a1ce95a771b32b0b12e8acd7756ec9cc1193d3e
jedr <andrzej.stencel@gmail.com>
2021-03-05 21:27:37 +0000

    macos: Add Oh My Zsh to work

--- macos/work.sh
+++ macos/work.sh
@@ -10,3 +10,15 @@ defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 
 brew install kubectl
 brew install k9s
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+## Copy .zshrc
+
+# Install Oh My Zash theme: powerlevel10k https://github.com/romkatv/powerlevel10k#get-started
+## Install font: https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k
+## Install Oh My Zsh: https://github.com/romkatv/powerlevel10k#oh-my-zsh
+
+# Add Oh My Zsh plugins: vagrant
+
