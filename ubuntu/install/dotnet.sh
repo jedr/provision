c6872fb63a6bfdc7d68d2c4f5296bb2afcbe0a7e
jedr <andrzej.stencel@gmail.com>
2021-11-15 15:19:18 +0100

    refactor: move installation scripts from vagrant to ubuntu

--- ubuntu/install/dotnet.sh
+++ ubuntu/install/dotnet.sh
@@ -3,5 +3,7 @@
 # Taken from https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#2004-
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-5.0
