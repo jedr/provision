8759f9717dfdc474c4d1e610a11231945df3771d
jedr <andrzej.stencel@gmail.com>
2022-04-19 11:55:07 +0200

    ubuntu: add syncthing install script

--- ubuntu/install/stern.sh
+++ ubuntu/install/stern.sh
@@ -7,4 +7,3 @@ curl -s https://api.github.com/repos/stern/stern/releases/latest |
   jq -r '.assets[] | select(.name | test("linux_amd")) | .browser_download_url' |
   xargs curl --location |
   tar -xzf - --directory ~/bin --strip-components=1 --wildcards 'stern*/stern'
-
---
+++ ubuntu/install/syncthing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+sudo curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt-get update
+sudo apt-get --yes install syncthing
