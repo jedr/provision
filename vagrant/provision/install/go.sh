cca5a68a3aa3c09e90527a5eb60af5d3d2cfbd5a
jedr <andrzej.stencel@gmail.com>
2022-04-19 11:52:10 +0200

    vagrant(go): Use .zshrc instead of .bashrc

--- vagrant/provision/install/go.sh
+++ vagrant/provision/install/go.sh
@@ -5,5 +5,5 @@ GO_VERSION="1.17.1"
 wget "https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz"
 sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
 rm go${GO_VERSION}.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
 PATH=$PATH:/usr/local/go/bin
